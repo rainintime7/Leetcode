3a2e50202ddea8e56055b2421ad38db46745b8ac
Wang Shu <wangsh256@outlook.com>
2022-04-06 14:48:15 +0200

    20220406

--- whiteborad.py
+++ whiteborad.py
@@ -2,3 +2,24 @@ L = ['a','a','b']
 
 print(''.join(L))
 
+class test:
+    def __init__(self) -> None:
+        return 
+
+a = test()
+a.pre = 1
+print(a.pre)
+
+# b = []
+
+# def fun():
+#     #global b 
+#     b.append(2)
+#     print(b)
+#     b = [1,2]
+    
+# fun()
+
+# print(b)
+
+print([0]*3)
